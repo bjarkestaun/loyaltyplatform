2022cca8d73280b7d4a4a72789d461201be20d2b
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-02-15 23:03:11 +0100

    Lars er finally sej

--- deploy_production.sh
+++ deploy_production.sh
@@ -2,4 +2,4 @@
 set -e
 grunt build
 scp -Cr ./dist web@54.93.92.239:/opt/web
-ssh web@54.93.92.239 'export NODE_ENV=production && export PORT=8080 && cd /opt/web/dist && npm update --production && naught start /opt/web/dist/server/app.js && naught deploy'
+ssh web@54.93.92.239 'export NODE_ENV=production && export PORT=8080 && cd /opt/web/dist && npm update --production && naught deploy'
